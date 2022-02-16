8637263901c6632e2096df011d11216379c6760c
Michael Claborn <mclaborn2010@gmail.com>
2022-02-16 12:31:39 -0600

    added card and comment details

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import axios from "axios";
 
 const App = () => {
   const authors = [
@@ -18,38 +20,41 @@ const App = () => {
     let randomAuthor = authors[Math.floor(Math.random() * authors.length)];
     return randomAuthor;
   }
+
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author={returnRandomAuthor()}
-        timeAgo="Today at 4:15PM"
-        text="Great Job!"
-        src={faker.image.image()}
-      />
-      <CommentDetail
-        author={returnRandomAuthor()}
-        timeAgo="Yesterday at 5:55PM"
-        text="Loved the food!"
-        src={faker.image.image()}
-      />
-      <CommentDetail
-        author={returnRandomAuthor()}
-        timeAgo="Today at 2:46PM"
-        text="It was ok I guess.."
-        src={faker.image.image()}
-      />
-      <CommentDetail
-        author={returnRandomAuthor()}
-        timeAgo="Yesterday at 7:42PM"
-        text="Wow! Just wow!"
-        src={faker.image.image()}
-      />
-      <CommentDetail
-        author={returnRandomAuthor()}
-        timeAgo="Today at 12:35PM"
-        text="meh"
-        src={faker.image.image()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={returnRandomAuthor()}
+          timeAgo="Today at 4:15PM"
+          text="Great Job!"
+          src={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={returnRandomAuthor()}
+          timeAgo="Yesterday at 5:55PM"
+          text="Loved the food!"
+          src={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={returnRandomAuthor()}
+          timeAgo="Today at 2:46PM"
+          text="It was ok I guess.."
+          src={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
