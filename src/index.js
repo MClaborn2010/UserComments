0c5ebce1652291b654abe07dac3b9eb4ea1836e3
Michael Claborn <mclaborn2010@gmail.com>
2022-02-15 19:21:14 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const authors = [
+    "Michael",
+    "Sarah",
+    "Daniel",
+    "Jana",
+    "Mark",
+    "Rejean",
+    "Malerie",
+  ];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  function returnRandomAuthor() {
+    let randomAuthor = authors[Math.floor(Math.random() * authors.length)];
+    return randomAuthor;
+  }
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author={returnRandomAuthor()}
+        timeAgo="Today at 4:15PM"
+        text="Great Job!"
+        src={faker.image.image()}
+      />
+      <CommentDetail
+        author={returnRandomAuthor()}
+        timeAgo="Yesterday at 5:55PM"
+        text="Loved the food!"
+        src={faker.image.image()}
+      />
+      <CommentDetail
+        author={returnRandomAuthor()}
+        timeAgo="Today at 2:46PM"
+        text="It was ok I guess.."
+        src={faker.image.image()}
+      />
+      <CommentDetail
+        author={returnRandomAuthor()}
+        timeAgo="Yesterday at 7:42PM"
+        text="Wow! Just wow!"
+        src={faker.image.image()}
+      />
+      <CommentDetail
+        author={returnRandomAuthor()}
+        timeAgo="Today at 12:35PM"
+        text="meh"
+        src={faker.image.image()}
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
